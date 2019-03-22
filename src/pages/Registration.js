3482fffdb8fc696ab97a65595b1c06d1c381e758
YriyFinogenov <giropoh@gmail.com>
2019-03-22 23:56:07 +0200

    registartion

---
+++ src/pages/Registration.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Registration extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="registrationPageContainer">
+
+            </div>
+        )
+    }
+}
+
+export default Registration;
